f41399e296a4ae26afba09b4bcc158d059f2bf1e
ethanHFU <102140406+ethanHFU@users.noreply.github.com>
2022-06-20 21:27:22 +0200

    algo

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const timer_el = document.getElementById("timer");
 const start_btn = document.getElementById("playWhite");
 const gamePad = document.getElementById("gameDisplay");
 
+ 
+
 
 let seconds = 0;
 let interval = null;
 
 // Event listeners 
-start_btn.addEventListener("click", start);
+//start_btn.addEventListener("click", start);
 
 
 // Update timer
@@ -33,9 +35,10 @@ function start(){
     interval = setInterval(timer, 1000)
 }
 
-
+//game starts
 start_btn.addEventListener("click", ()=>{start_btn.style.display = "none";});
 start_btn.addEventListener("click", ()=>{createGamePad()});
+const placements = [];
 start_btn.addEventListener("click", startGame);
 
 
@@ -43,13 +46,15 @@ start_btn.addEventListener("click", startGame);
 
 
 
-let canvas = document.createElement("canvas");
+const canvas = document.createElement("canvas");
 let ctx = canvas.getContext("2d");
 
-canvas.width = 1025;
-canvas.height = 400;
+
+canvas.height = screen.height * (0.85*0.7);
+canvas.width = screen.width * (0.7);
 canvas.style.borderRadius = "1.5rem";
 canvas.style.border = "2px solid black";
+
 
 
 
@@ -58,7 +63,7 @@ function createGamePad(){
     gamePad.appendChild(canvas);
 }
 
-let posX = canvas.width/2;
+let posX = 35;
 let posY = canvas.height/2;
 
 
@@ -66,35 +71,35 @@ let posY = canvas.height/2;
 let dx = 0;
 let dy = 0;
 
-
+let speed = 2;
 
 document.addEventListener("keydown", direction);
-document.addEventListener("keyup", ()=>{dx = 0; dy = 0;});
+
 
 function direction(e) {
     if(e.code === 'KeyD') {
         if(posX < canvas.width - 10){
-            dx = 0.5;
+            dx = speed;
             dy = 0;
         }
     }
     else if(e.code === 'KeyA') {
         if(posX != 0 + 10){
-            dx = -0.5;
+            dx = -speed;
             dy = 0;
         }
     }
     else if(e.code === 'KeyW') {
         if(posY != 0 + 10){
             dx = 0;
-            dy = -0.5;
+            dy = -speed;
         }
     }
 
     else if(e.code === 'KeyS') {
         if(posY != canvas.height - 10){
             dx = 0;
-            dy = 0.5;
+            dy = speed;
         }
     }
 
@@ -103,7 +108,7 @@ function direction(e) {
 
 
 
-function drawBarrierTop(x, y, width, height){
+function drawWall(x, y, width, height){
     ctx.beginPath();
     ctx.rect(x, y, width, height);
     ctx.fillStyle = "#000";
@@ -111,15 +116,13 @@ function drawBarrierTop(x, y, width, height){
     ctx.closePath();
 }
 
-function drawBarrierBottom(x, y, width, height){
-    ctx.rect(x, y - height, width, height);
-}
+
 
 
 
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(posX, posY, 10, 0, Math.PI*2);
+    ctx.arc(posX, posY, 25, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -129,8 +132,17 @@ function drawBall(){
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    drawBarrierTop(30, 0, 30, 200);
     
+    for(let i = 0; i < placements.length; i++){
+        
+        drawWall(placements[i].x, 0, 20, placements[i].pos);
+        drawWall(placements[i].x, placements[i].pos + placements[i].opening, 20, canvas.height - placements[i].pos - placements[i].opening);
+
+    }
+
+    
+
+
     if((posX + dx) >= 10 && (posX + dx) <= canvas.width - 10){
         posX += dx;
     }
@@ -138,13 +150,47 @@ function draw() {
     if ((posY + dy) >= 10 && (posY + dy) < canvas.height - 10){
         posY += dy;
     }
+
     
 }
 
 // call draw every x ms 
 function startGame(){
     gameInterval = setInterval(draw, 1);
+    generateWallInfo(5, 50, 100);
 }
+
+ctx.getImageData
+
+function generateWallInfo(segments, minOpening, maxOpening){
+
+    
+
+    for(let i = 0; i < segments; i++){
+        
+        let delta = canvas.width/segments;
+
+        let openingSize = getRndInteger(minOpening, maxOpening);
+        
+
+        let openingY = getRndInteger(0, canvas.height - openingSize);
+
+
+
+        const wall = {x:(i + 1)*delta, opening:openingSize, pos:openingY};
+
+        placements[i] = wall;
+
+    }
+
+}
+
+function getRndInteger(min, max) {
+    let x = Math.floor(Math.random() * (max - min) ) + min;
+    
+
+    return x;
+  }
 
 
 
